3ad5f6a22ea0bce567f95701f81e8d740ef23f8a
phucphan121200 <phanphuc121200@gmail.com>
2021-11-01 11:34:46 +0700

    register without email

--- api/services/userService.js
+++ api/services/userService.js
@@ -1,18 +1,14 @@
 "use strict";
-const CryptoJS = require("crypto-js");
 const User = require("../models/user");
 
 class userService {
     static async registration(data) {
-        const newUser = new User({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            dob: req.body.dob,
-            password: CryptoJS.AES.encrypt(
-              req.body.password,
-              process.env.SECRET_KEY
-            ).toString(),
+        const userData = new User({
+            firstname: data.firstname,
+            lastname: data.lastname,
+            email: data.email,
+            dob: data.dob,
+            password: data.password,
         });
         return await User(userData).save();
       }
@@ -21,3 +17,4 @@ class userService {
         return await User.findOne({ email });
     }
 }
+module.exports = userService;
