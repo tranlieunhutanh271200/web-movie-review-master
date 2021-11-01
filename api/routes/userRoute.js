91b9e9ad88867633d1d5a83c765b45c4a83b7490
phucphan121200 <phanphuc121200@gmail.com>
2021-11-01 19:49:38 +0700

    Update User

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,4 +1,6 @@
+"use strict";
 const router = require("express").Router();
+const verifyToken = require("../middlewares/verifyToken");
 
 const jwt = require("jsonwebtoken");
 const userController = require("../controllers/userController")
@@ -7,5 +9,7 @@ const userController = require("../controllers/userController")
 router.post("/register", userController.registration);
 //LOGIN
 router.post("/login", userController.login);
+//UPDATE
+router.put("/update/:id", verifyToken, userController.update);
 
 module.exports = router;
